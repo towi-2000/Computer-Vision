6d9100fb285c152edda95e52445024f5bd67ff45
towi2000 <69476612+towi-2000@users.noreply.github.com>
2026-09-07 18:02:35 +0200

    debug

--- main.py
+++ main.py
@@ -190,9 +190,7 @@ def measure_distance(trigger:int, echo:int, timeout=0.03):
 #----------------------------
 
 #image procession
-cam_index = find_camera()
-if cam_index is None: raise RuntimeError("Keine Kamera gefunden")
-cam = cv.VideoCapture(cam_index)
+cam = cv.VideoCapture(0)
 
 #aruco marker
 aruco_dict = cv.aruco.getPredefinedDictionary(aruco_type)
