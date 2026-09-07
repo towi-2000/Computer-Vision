56a9aedf7f1fea52a487387ad4056c9116bf9b7e
towi2000 <69476612+towi-2000@users.noreply.github.com>
2026-09-07 16:06:45 +0200

    debug

--- main.py
+++ main.py
@@ -219,10 +219,6 @@ time.sleep(0.5)
 i2c = SMBus(1) #SDA: 3, SCL: 5
 i2c.write_byte_data(0x70, 0x00, 0x01)
 i2c.write_byte_data(0x70, 0xE8, 0xAA)
-#drive(255,0,255,0) #vorwärts
-#time.sleep(10)
-#drive(0,0,0,0)
-#raise SystemExit
 
 #----------------------------
 # while-loop
@@ -235,7 +231,7 @@ while cam.isOpened():
     #measure distance
     if time.time() - state.last_measurement > state.measurement_time:
         distance = measure_distance(state.trigger, state.echo, state.timeout_factor)
-        # distance = sensor.distance * 100
+        print(f"Distanz: {distance}")
         if distance is not None: 
             state.distances.append(distance)
             state.dist = np.median(state.distances)
@@ -268,7 +264,7 @@ while cam.isOpened():
         state.cy = int(np.mean(pts[:,1]))
 
         #print(f"Mittelpunkt: ({state.cx}, {state.cy})")
-        print(f"Distanz: {state.dist}")
+        # print(f"Distanz: {state.dist}")
 
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
